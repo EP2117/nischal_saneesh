b169014432c4157db32f0c9274b307e0a20457f0
kaungsattkhant <ookuang42@gmail.com>
2020-10-07 18:07:11 +0630

    mg

--- app/Payment.php
+++ app/Payment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     protected $table='payments';
-    protected $fillable=['date','cash_payment_no','debit_id','credit_id','amount','remark'];
+    protected $fillable=['date','cash_payment_no','debit_id','credit_id','amount','remark','update_at','created_at','updated_by','created_by'];
     protected $with=['debit','credit'];
     public function debit(){
         return $this->belongsTo(SubAccount::class,'debit_id','id');
