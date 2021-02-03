82b713af6a19ebd6b4ebd99c8b0a0aa5f138cd8b
kaungsattkhant <ookuang42@gmail.com>
2021-02-03 16:51:18 +0630

    order form done

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -321,9 +321,9 @@ class OrderController extends Controller
        // $pdf = PDF::loadView('invoice_print', $data);
        // if(Auth::user()->role->id == "1" || Auth::user()->role->id == "2") {
             $order = Order::with('products','warehouse','customer','products.uom','products.selling_uoms')->find($order_id);  
+            return view('exports.order_print', compact('order'));
             $pdf = PDF::loadView('exports.order_print', compact('order'));
             $output = $pdf->output();
-
             return new Response($output, 200, [
                'Content-Type' => 'application/pdf',
                'Content-Disposition' =>  'inline; filename="order.pdf"',
